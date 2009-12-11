95267da05b7452cdcdb4fe9f45f2d6f66754f73a
liliana <liliana@leia.(none)>
2009-12-10 22:11:01 -0430

    subiendo a las 1010 pm (Na de Na)

--- semantica.py
+++ semantica.py
@@ -4,10 +4,6 @@ from excepcion import *
 sys.setrecursionlimit(3000)
 # Match
 def match(nodo1,nodo2):
-	#print 'matching'
-	#print nodo1
-	#print nodo2
-	# Fin de recursion
 	if (not isinstance(nodo1,Nodo.Nodo)) and (not isinstance(nodo2,Nodo.Nodo)):
 		return nodo1 == nodo2
 
@@ -40,7 +36,24 @@ def match(nodo1,nodo2):
 		return comparar_listas(nodo1,nodo2,[])
 
 	return False
-
+def pa(nodo,env):
+	a= []
+	if isinstance(nodo,Nodo.Nodo):
+		if nodo.type == 'LISTAPATRON':
+			pa(nodo.izquierdo,env)
+		if nodo.type == 'lp':
+			if isinstance(nodo.derecho,Nodo.Nodo):
+				print valor(eval(nodo.derecho,env))
+				a.append(valor(eval(nodo.derecho,env)))
+				if isinstance(nodo.izquierdo,Nodo.Nodo):
+					pa(nodo.izquierdo,env)
+				else: 
+					print (valor(eval(nodo.derecho)))
+			else:
+				print valor(eval(nodo,env))
+		else: print a
+			
+	
 # Comparar Listas
 def comparar_listas(lista1,lista2,tuplas):
 	if match(lista1.izquierdo,lista2.izquierdo):
@@ -80,17 +93,19 @@ def extend(diccionario,clave,valor):
 	diccionario[clave] = valor
 	return diccionario
 
-# Lookup
+# lookup
 def lookup(clave,diccionario):
 	try:
 		if clave in diccionario:
-			return diccionario[clave]
-		else: raise ParametrosError('Variable '+str(clave)+' no declarada')
+			if diccionario[clave]=='fake':
+				raise ParametrosError('De recursion')
+			else: return diccionario[clave]
+		else:
+			raise ParametrosError('De lookup')
 	except ParametrosError, messag:
 		messag = messag.messg
 		print 'Error : ' + messag
 			
-
 # Eval
 def eval(nodo,env,orientacion):
 	if orientacion == 'izquierda': return eval(nodo.izquierdo,env)
@@ -108,26 +123,23 @@ def valor(nodo):
 			return str(valor(nodo.izquierdo))+'::'+str(valor(nodo.derecho))
 	return nodo
 
-# Cantidad de patrones de una Funcion
-def cantidad_patrones(nodo):
-	while (nodo.type != 'lp'):
-		nodo = nodo.izquierdo
-	global suma
-	suma = 0
-	tam_listapatron(nodo)
-	return suma
+
 	
 # Apply
-def apply(cls,nodo):
-	for c in cls.clausura:
-		comparar = match(c[0],nodo)
-		if comparar:
-			if isinstance(comparar,list):
-				nuevo_env = copy.deepcopy(cls.env)
-				for n in comparar:				
-					extend(nuevo_env,valor(n[0]),n[1])
-				return eval(c[1],extend(nuevo_env,str(valor(c[0])),nodo))
-			else : return eval(c[1],extend(copy.deepcopy(cls.env),str(valor(c[0])),nodo))
+def apply(cls,nodo): 
+	if isinstance(cls,CLS.CLS):
+		for c in cls.clausura:
+			comparar = match(c[0],nodo)
+			if comparar:
+				if isinstance(comparar,list):
+					nuevo_env = copy.deepcopy(cls.env)
+					for n in comparar:				
+						extend(nuevo_env,valor(n[0]),n[1])
+					return eval(c[1],extend(nuevo_env,str(valor(c[0])),nodo))
+				else : return eval(c[1],extend(copy.deepcopy(cls.env),str(valor(c[0])),nodo))
+	#Error de aplicar una No funcion
+	else: raise ParametrosError('De aplicacion') 
+	#Error de matching
 	raise ParametrosError(' De matching')
 
 	
@@ -141,180 +153,6 @@ def clausura(nodo,env,temp):
 	return CLS.CLS(env,temp)
 
 
-# Obtener patrones de una lista de patrones
-def patrones(nodo,listap):
-	#if isinstance(nodo,Nodo.Nodo):
-		#if nodo.type == 'PATRON':
-			#listap.append(nodo)
-		#if isinstance(nodo.izquierdo,Nodo.Nodo):
-			#patrones(nodo.izquierdo,listap)
-		#if isinstance(nodo.derecho,Nodo.Nodo):
-			#patrones(nodo.derecho,listap)
-	if nodo.izquierdo.izquierdo.type == 'PATRON':
-		primer_patron = nodo.izquierdo.izquierdo #copy.deepcopy(nodo.izquierdo.izquierdo)
-		nodo.izquierdo = nodo.derecho # copy.deepcopy(nodo.derecho)
-		nodo.derecho = ''
-		return (primer_patron, nodo)
-	else:
-		print 'PATRONES',nodo,nodo.izquierdo.izquierdo
-		abuelo,papa,hijo = nodo,nodo.izquierdo,nodo.izquierdo.izquierdo
-
-		while hijo.type != 'PATRON':
-			print hijo.izquierdo
-			abuelo = papa
-			papa = hijo
-			hijo = hijo.izquierdo
-		primer_patron = hijo.izquierdo
-		papa.izquierdo = papa.derecho #copy.deepcopy(papa.derecho)
-		papa.derecho = ''
-		return (primer_patron,nodo)
-
-	#return listap
-
-# Obtener cuerpo (listas de patrones y expresiones)
-# de una funcion
-def cuerpo(nodo,body):
-	if isinstance(nodo,Nodo.Nodo):
-		if nodo.type == 'lfe':
-			body.append((patrones(nodo.izquierdo,[]),nodo.derecho))
-		cuerpo(nodo.izquierdo,body)
-		cuerpo(nodo.derecho,body)
-	return body
-
-# Lista de patrones sin el primero
-def resto_de_patrones(nodo):
-	if nodo.izquierdo.izquierdo.type == 'PATRON':
-		nodo.izquierdo.izquierdo = copy.deepcopy(nodo.derecho)
-		nodo.derecho = ''
-		return nodo
-	else:
-		abuelo,papa,hijo = nodo,nodo.izquierdo,nodo.izquierdo.izquierdo
-		while hijo.izquierdo != 'PATRON':
-			abuelo = papa
-			papa = hijo
-			hijo = hijo.izquierdo
-		papa.izquierdo = copy.deepcopy(papa.derecho)
-		papa.derecho = ''
-		return nodo
-
-# Factorizar funcion
-def factorizar(body):
-	conjunto = []
-	particion = {}
-	clave = {}
-	exp = {}
-	pat = {}
-	p,q,i,j,k = 0,1,0,0,0
-	#print 'body',body
-	for b in body:
-		#print 'b',b[0][1]
-		conjunto.append(b[0][0])
-	clave[0] = 0
-	if len(conjunto) == 1:
-		particion[0] = [conjunto[0]]
-		exp[0] = [body[0][1]]
-		#pat[0] = [body[0][0][1:len(body[0][0])]]
-		pat[0] = [body[0][0][1]]#
-		#pat[0] = resto_de_patrones(body[0][0])
-	print 'len' ,len(conjunto)
-	while p < len(conjunto):
-
-		print 'p,q,clave: ',p,q,clave
-		q = p + 1
-		#if p not in clave:
-		#particion[q] = [conjunto[0]]
-	       	#exp[q] = [body[0][1]]
-	       	while q < len(conjunto):
-			print 'ppp',pat
-	       		if match(conjunto[p],conjunto[q]) and match (conjunto[q],conjunto[p]):
-	       			print '# clave',clave
-	       			print '# particion',particion
-	       			print '# exp', exp
-	       			print '# p', p
-	       			print '# q',q
-	       		#print 'conjunto',conjunto[p],conjunto[q],p,q
-	       			clave[q] = p
-	       			if p in particion and conjunto[q] not in particion[p]:
-	       				particion[p].append(conjunto[q])
-					#pat[p].append(body[p][0][1:len(body[p][0])])#
-					pat[p].append(body[p][0][1])
-					#pat[p].append(resto_de_patrones(body[p][0]))
-	       			else:
-	       				particion[p] = [conjunto[q]]
-					#pat[p] = body[p][0][1:len(body[p][0])]#
-					pat[p] = [body[p][0][1]]
-					#pat[p] = resto_de_patrones(body[p][0])
-	       			if p in exp and body[q][1] not in exp[p]:
-	       				print 'abcdefg'
-	       				exp[p].append(body[q][1])
-					if len(body[q][0]) == 1:
-						#pat[p].append(body[q][0])#
-						pat[p].append(body[q][0][1])
-						#pat[p].append(resto_de_patrones(body[q][0]))
-					else:
-						#pat[p].append(body[q][0][1:len(body[q][0])])#
-						pat[p].append(body[q][0][1])
-						#pat[p].append(resto_de_patrones(body[q][0]))
-	       			else:
-	       				exp[p] = [body[q][1]]
-					if len(body[q][0]) == 1:
-						#pat[p] = body[q][0]#
-						pat[p] = [body[q][0][1]]
-						#pat[p] = resto_de_patrones(body[q][0])
-					else:
-						#pat[p] = body[q][0][1:len(body[q][0])]#
-						pat[p] = [body[q][0][1]]
-						#pat[p] = resto_de_patrones(body[q][0])
-	       			exp[p].append(body[p][1])
-				if len(body[p][0]) == 1:
-					#pat[p].append(body[p][0])#
-					pat[p].append(body[p][0][1])
-					#pat[p].append(resto_de_patrones(body[p][0]))
-				else:
-					#pat[p].append(body[p][0][1:len(body[p][0])])#
-					pat[p].append(body[p][0][1])
-					#pat[p].append(resto_de_patrones(body[p][0]))
-	       		q += 1
-		p += 1
-	
-	#print 'PARTICION\n *',particion, '\nEXP\n *',exp
-	print 'PARTICION\n *',particion,'\n *',particion[0][0] ,'\n *', particion[1][0] ,'\nEXP\n *',exp,'\n *', exp[0][0],'\n *', exp[0][1],'\n *', exp[1][0],'\n *', exp[1][1]#,'\n *', exp[2][0]
-	#print 'PARTICION\n *',particion,'\n *',particion[0][0],'\n *', particion[0][1] ,'\n *',particion[0][2],'\n *','\nEXP\n *',exp,'\n *', exp[0][0],'\n *', exp[0][1] ,'\n *', exp[0][1]#,'\n *', exp[2][0]
-	#i = 0
-	#j = 0
-	#k = 0
-#	print '\nPAT\n *',pat#,'\n *', pat[0][0],'\n *', pat[0][1][0],'\n *', pat[1][0],'\n *', pat[1][1]
-	print 'PAT\n *', pat, '\n *',pat[0][0],'\n *', pat[0][1],'\n *', pat[1][0],'\n *', pat[1][1]#
-#	print pat[0][0],pat[1][0]	
-	print conjunto[0], conjunto[1]
-	factorizada = Nodo.Nodo('FUN',0)
-	arboles = []
-	while i < len(particion):
-		j = 0
-		
-		while j < len(exp[i]):
-			if j == 0:
-				temp = Nodo.Nodo('FUN',Nodo.Nodo('arg',Nodo.Nodo('arg2',Nodo.Nodo('lfe',pat[i][j],exp[i][j]))))
-				arg = temp.izquierdo
-			else:
-				arg.derecho = Nodo.Nodo('arg',Nodo.Nodo('arg2',Nodo.Nodo('lfe',pat[i][j],exp[i][j])))
-				arg = arg.derecho
-			j += 1
-		arboles.append(temp)
-		i += 1
-	
-	
-	while k < len(particion):
-		if k == 0:
-			temp = Nodo.Nodo('FUN',Nodo.Nodo('arg',Nodo.Nodo('arg2',Nodo.Nodo('lfe',Nodo.Nodo('LISTAPATRON',conjunto[k]),arboles[k]))))
-			arg = temp.izquierdo
-		else:
-			arg.derecho = Nodo.Nodo('arg',Nodo.Nodo('arg2',Nodo.Nodo('lfe',Nodo.Nodo('LISTAPATRON',conjunto[k]),arboles[k])))
-			arg = temp.derecho
-		k += 1
-	return temp
-				    
-# Eval
 def es_entero(x,y):
 	if isinstance(x,int) and isinstance(y,int) and not(isinstance(x,bool)) and not(isinstance(y,bool)):
 		return True
@@ -335,15 +173,17 @@ def eval(nodo,env):
 		if nodo.type == 'arg2': return eval(nodo.izquierdo,env)
 		if nodo.type == 'lp':return nodo
 		elif nodo.type == 'FUN': 
+			
 			#print 'In-Fun\n', cuerpo(nodo,[])
 			#cuerpo_fun = cuerpo(nodo,[])
 			#if len(cuerpo_fun[0][0]) != 1:
-				#print 'CUERPO',cuerpo_fun[0][0]
-				#factorizada = factorizar(cuerpo_fun)
-				#return eval(factorizada,env)
-				#fun_factorizada = factorizar(nodo)
+			#	print 'CUERPO',cuerpo_fun[0][0]
+			#	factorizada = factorizar(cuerpo_fun)
+			#	return eval(factorizada,env)
+			#	fun_factorizada = factorizar(nodo)
 			#else:
 			return clausura(nodo,env,[])
+			
 			#return eval(nodo.izquierdo,env)
 		elif nodo.type == 'LISTAPATRON': return nodo
 		elif nodo.type == 'no_terminal': return eval(nodo.izquierdo,env)
@@ -444,11 +284,10 @@ def eval(nodo,env):
 			else: raise ParametrosError('De tipo en los parametros del <>')
 		elif nodo.type == 'NO' :
 			i = valor(eval(nodo.izquierdo,env))
-			d = valor(eval(nodo.derecho,env))
-			if es_booleano(i,d):
-				resultado = (i or d)
+			if es_booleano(i,True):
+				resultado = not(i)
 				return Nodo.Nodo('CONSTANTE',Nodo.Nodo('BOOLEANO',str(resultado).upper()))
-			else: raise ParametrosError('De tipo en los parametros del OR')
+			else: raise ParametrosError('De tipo en la Negacion')
 		elif nodo.type == 'OR' :
 			i = valor(eval(nodo.izquierdo,env))
 			d = valor(eval(nodo.derecho,env))
@@ -473,7 +312,7 @@ def eval(nodo,env):
 			p = nodo.izquierdo.izquierdo
 			e1 = nodo.izquierdo.derecho
 			e2 = nodo.derecho
-			env1 = extend(env,p,'fake')
+			env1 = extend(copy.deepcopy(env),str(valor(p)),'fake')
 			v1 = eval(e1,env1)
 			return eval(e2,replace(env1,str(valor(p)),v1))
 		
@@ -486,7 +325,7 @@ def eval(nodo,env):
 			return
 		elif nodo.type == 'APLICAR':
 				return apply(eval(nodo.izquierdo,env),eval(nodo.derecho,env))
-			#else: raise ParametrosError('De aplicacion')
+			
 	# Manejador de excepciones
 	except ParametrosError, messag:
 		messag = messag.messg
